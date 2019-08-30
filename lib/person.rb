67483e46058c49560102b1cd355cdecb752590d4
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-30 04:13:17 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+def name=(person_name)
+  @this_persons_name =persons_name
+end 
+def name 
+  @this_persons_name
+  end 
+end 
+
+class Job
+def job=(persons_job)
+  @this_persons_job=persons_job
+end 
+def job 
+  @this_persons_job
+  end 
+end 
