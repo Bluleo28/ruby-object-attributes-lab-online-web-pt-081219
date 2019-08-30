746d873f9472bd6d6c78ff61d7e92ce3155d5e9f
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-30 04:45:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,15 +3,15 @@ def name=(person_name)
   @this_persons_name =persons_name
 end 
 def name =(persons_name) 
-    @name
+    @name = (persons_name)
   end 
 end 
 
 class Person
 def job=(persons_job)
-  @this_persons_job=persons_job
+  @this_persons_job= (persons_job)
 end 
 def job =(persons_job) 
-    @job
+    @job = (persons_job)
   end 
 end 
