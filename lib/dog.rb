17106de03c1d9a57dd88308486b7c52414e8f2b0
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-30 05:15:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,17 +4,17 @@ def name=(dog_name)
 end
 
 def name=(dog_breed)
-    @name =(dog_breed)
+    @dog_name =(dog_breed)
   end 
   
 def name=(dog_name) 
-    @name =("Snoopy")
+    @dog_name =(dog_name)
   end 
 end 
 
 class Dog 
 def breed=(dog_breed)
-    @breed =("Beagle")
+    @breed =(dog_breed)
 end 
 def breed
     @breed 
